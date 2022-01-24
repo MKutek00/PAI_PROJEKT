3508e5d2f43172bf4324996e79999e6ebcf373b1
Mateusz Kutek <zmati999@gmail.com>
2022-01-24 20:33:10 +0100

    First changes

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+const USERNAME = 'zkegembulqfxwh';
+const PASSWORD = 'bbcd024e7ffd68222e77a5676ef03395e2183cf6fd5c56824aec253903fc319c';
+const HOST = 'ec2-52-19-164-214.eu-west-1.compute.amazonaws.com';
+const DATABASE = 'd4ulp39ebf55hf';
--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -2,19 +2,26 @@
 
 require_once 'AppController.php';
 require_once __DIR__ .'/../models/User.php';
+require_once __DIR__.'/../repository/UserRepository.php';
+
 
 class SecurityController extends AppController {
 
     public function login()
-    {   
-        $user = new User('gutek@gmail.com', 'admin', 'Gutek');
+    {
+        $userRepository = new UserRepository();
 
-        if (!$this->isPost()) {
+        if(!$this->isPost()){
             return $this->render('login');
         }
 
         $email = $_POST['email'];
         $password = $_POST['password'];
+
+        $user = $userRepository->getUser($email);
+        if(!$user){
+            return $this->render('login', ['messages' =>['Brak użytkownika w bazie danych']]);
+        }
 
         if ($user->getEmail() !== $email) {
             return $this->render('login', ['messages' => ['E-mail nie znaleziony w bazie danych!']]);
